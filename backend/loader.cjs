8c8643f22b93efb0f043e87af4cf9d2c41d907f0
duyphuc0701 <duyphuc070104@gmail.com>
2026-07-28 21:38:23 +0700

    Add loader file

---
+++ backend/loader.cjs
@@ -0,0 +1,8 @@
+// Source - https://stackoverflow.com/a/78399914
+// Posted by Buchi
+// Retrieved 2026-07-28, License - CC BY-SA 4.0
+
+async function loadApp() {
+    await import('./index.js');
+}
+loadApp();
