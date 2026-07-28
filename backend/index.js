41ffc781209d14f4947f54ce8fd4bf31a37d56a5
maixuanbach174 <22125005@student.hcmus.edu.vn>
2026-07-28 22:17:04 +0700

    remove require main

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const { startServer } = require('./src/server');
 
-if (require.main === module) {
-  startServer();
-}
+startServer();
 
 module.exports = { startServer };
+
--- backend/src/server.js
+++ backend/src/server.js
@@ -18,3 +18,4 @@ function startServer({
 }
 
 module.exports = { startServer };
+
